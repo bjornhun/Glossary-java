d47dd695d6189648bbd8b165ab72eb49284e98fc
Bjørnar Hunshamar <bjornarhuns@gmail.com>
2018-06-20 12:34:31 +0200

    Add WordList class

---
+++ src/com/bjornar/glossary/WordList.java
@@ -0,0 +1,17 @@
+package com.bjornar.glossary;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class WordList {
+    ArrayList<WordListItem> items = new ArrayList<WordListItem>();
+
+    public WordList(String filename) {
+
+    }
+
+    private readFromFile(String filename) throws FileNotFoundException {
+        /*BufferedReader in = new BufferedReader(i);*/
+    }
+}
